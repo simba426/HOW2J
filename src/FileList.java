b23758454ab7b713aaeaa4f070dcfcb31571d43c
simba426 <isxinzq@hotmail.com>
2019-06-29 18:47:41 +0800

    scan all files under directory through an iterative process

--- src/FileList.java
+++ src/FileList.java
@@ -1,27 +1,33 @@
 import java.io.File;
 
 public class FileList {
-    public static void run() {
-        File f = new File("/Users/simba/Documents");
-        File[] files = f.listFiles();
-        if (null == files)
-            return;
-        long minLength = Integer.MAX_VALUE;
-        long maxLength = 0;
-        File maxFile = null;
-        File minFile = null;
-        for (File file: files) {
-            if (file.isDirectory())
-                continue;
-            if (file.length() > maxLength) {
-                maxLength = file.length();
-                maxFile = file;
+    private long minLength = Integer.MAX_VALUE;
+    private long maxLength = 0;
+    private File maxFile = null;
+    private File minFile = null;
+
+    public void maxAndMin(File f) {
+        if (f.isFile()) {
+            if (f.length() > maxLength) {
+                maxLength = f.length();
+                maxFile = f;
             }
-            if (file.length() != 0 && file.length() < minLength) {
-                minLength = file.length();
-                minFile = file;
+            if (f.length() != 0 && f.length() < minLength) {
+                minLength = f.length();
+                minFile = f;
             }
         }
+
+        if (f.isDirectory()){
+            File[] files = f.listFiles();
+            if (null != files)
+                for (File file: files) {
+                    maxAndMin(file);
+                }
+        }
+    }
+
+    public void result() {
         System.out.printf("最大的文件是%s,其大小是%d字节", maxFile.getAbsolutePath(), maxLength);
         System.out.printf("最小的文件是%s,其大小是%d字节", minFile.getAbsolutePath(), minLength);
     }
